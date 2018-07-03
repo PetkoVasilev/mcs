2a085aef0349fd7ce202543a2f8d9ea06d14b5f3
Petko Vasilev <petko.vasilev@mariadb.com>
2018-07-03 12:37:13 +0300

    added both kubernetes and docker-compose versions

--- um/postCfg2.sh
+++ um/postCfg2.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 export USER=root
+# for kubernetes dns
+export PM1_NODE_IP="$(host mariadb-cs-pm1 | awk '/has address/ { print $4 ; exit }')"
+export PM2_NODE_IP="$(host mariadb-cs-pm2 | awk '/has address/ { print $4 ; exit }')"
+export UM_NODE_IP="$(host mariadb-cs-um | awk '/has address/ { print $4 ; exit }')"
 
 # postConfigure with input passed in
 /bin/echo -e "$1" | /usr/local/mariadb/columnstore/bin/postConfigure
 
 # update root user to allow external connection, need to turn off NO_AUTO_CREATE_USER. 
-ssh -vvv -l root ${MARIADB_CS_UM_SERVICE_HOST} '/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'"'"'%'"'"';FLUSH PRIVILEGES;"'
+ssh -vvv -l root ${UM_NODE_IP} '/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'"'"'%'"'"';FLUSH PRIVILEGES;"'
 
 tail -f /dev/null
