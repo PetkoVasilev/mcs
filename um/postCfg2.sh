887db112bf99489e2e7a70217b42524599062bae
Petko Vasilev <petko.vasilev@mariadb.com>
2018-07-03 11:32:20 +0300

    changed env variables names

--- um/postCfg2.sh
+++ um/postCfg2.sh
@@ -5,6 +5,6 @@ export USER=root
 /bin/echo -e "$1" | /usr/local/mariadb/columnstore/bin/postConfigure
 
 # update root user to allow external connection, need to turn off NO_AUTO_CREATE_USER. 
-ssh -vvv -l root ${UM_NODE_IP} '/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'"'"'%'"'"';FLUSH PRIVILEGES;"'
+ssh -vvv -l root ${MARIADB_CS_UM_SERVICE_HOST} '/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'"'"'%'"'"';FLUSH PRIVILEGES;"'
 
 tail -f /dev/null
