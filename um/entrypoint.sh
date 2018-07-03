21ef5b04f8dafd2c62b74caed4fed0d6a0fa2029
Petko Vasilev <petko.vasilev@mariadb.com>
2018-07-03 12:25:36 +0300

    fix for missing ping for kubernetes

--- um/entrypoint.sh
+++ um/entrypoint.sh
@@ -2,4 +2,4 @@
 
 /usr/sbin/sshd
 
-/install/postCfg2.sh "2\n1\nn\nn\ncolumnstore-1\n1\n1\nserver-um\n${MARIADB_CS_UM_SERVICE_HOST}\n\n2\nserver-pm1\n${MARIADB_CS_PM1_SERVICE_HOST}\n\n\nserver-pm2\n${MARIADB_CS_PM2_SERVICE_HOST}\n\n2\n\n\n"
+/install/postCfg2.sh "2\n1\nn\nn\ncolumnstore-1\n1\n1\nserver-um\n${MARIADB_CS_UM_SERVICE_HOST}\n1\n\n2\nserver-pm1\n${MARIADB_CS_PM1_SERVICE_HOST}\n1\n\n\nserver-pm2\n${MARIADB_CS_PM2_SERVICE_HOST}\n1\n\n2\n\n\n"
