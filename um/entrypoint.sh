f1353c2668d05ed9f0834a959823a515c1b2ec35
Petko Vasilev <petko.vasilev@mariadb.com>
2018-07-05 17:33:32 +0300

    fixes

--- um/entrypoint.sh
+++ um/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# for kubernetes dns
+export PM1_NODE_IP="$(host mariadb-cs-pm1 | awk '/has address/ { print $4 ; exit }')"
+export PM2_NODE_IP="$(host mariadb-cs-pm2 | awk '/has address/ { print $4 ; exit }')"
+export UM_NODE_IP="$(host mariadb-cs-um | awk '/has address/ { print $4 ; exit }')"
+
 /usr/sbin/sshd
 
 /install/postCfg2.sh "2\n1\nn\nn\ncolumnstore-1\n1\n1\nserver-um\n${UM_NODE_IP}\n\n2\nserver-pm1\n${PM1_NODE_IP}\n\n\nserver-pm2\n${PM2_NODE_IP}\n\n2\n\n\n"
--- um/postCfg2.sh
+++ um/postCfg2.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 export USER=root
-# for kubernetes dns
-export PM1_NODE_IP="$(host mariadb-cs-pm1 | awk '/has address/ { print $4 ; exit }')"
-export PM2_NODE_IP="$(host mariadb-cs-pm2 | awk '/has address/ { print $4 ; exit }')"
-export UM_NODE_IP="$(host mariadb-cs-um | awk '/has address/ { print $4 ; exit }')"
 
 # postConfigure with input passed in
 /bin/echo -e "$1" | /usr/local/mariadb/columnstore/bin/postConfigure
@@ -11,4 +7,5 @@ export UM_NODE_IP="$(host mariadb-cs-um | awk '/has address/ { print $4 ; exit }
 # update root user to allow external connection, need to turn off NO_AUTO_CREATE_USER. 
 ssh -vvv -l root ${UM_NODE_IP} '/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'"'"'%'"'"';FLUSH PRIVILEGES;"'
 
+# don't exit the process
 tail -f /dev/null
